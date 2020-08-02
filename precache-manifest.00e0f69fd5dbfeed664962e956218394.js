ff7b3538af9b7dfa47a0bcbda6dab4c0e2378126
Vichea Nath <48352653+vicheanath@users.noreply.github.com>
2020-08-02 21:49:57 +0700

    frist commit my site

---
+++ precache-manifest.00e0f69fd5dbfeed664962e956218394.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b21184e1d164bd553c27e6de7f199370",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d36b3b38c5248c15a46",
+    "url": "/static/css/main.7e3905d2.chunk.css"
+  },
+  {
+    "revision": "001c3f1cc238a7f0a1ba",
+    "url": "/static/js/2.1768474f.chunk.js"
+  },
+  {
+    "revision": "10017bfb08c3f52cf1c70a46d09c7c96",
+    "url": "/static/js/2.1768474f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d36b3b38c5248c15a46",
+    "url": "/static/js/main.780ba7de.chunk.js"
+  },
+  {
+    "revision": "27e277ca3f130b8f1fce",
+    "url": "/static/js/runtime-main.ad540005.js"
+  },
+  {
+    "revision": "f9a44e1ccb9f44cd4de1da8fe7f6f4d8",
+    "url": "/static/media/arrow-left.f9a44e1c.svg"
+  },
+  {
+    "revision": "dc372a07777e87891aaac087cfbe60ad",
+    "url": "/static/media/arrow-right.dc372a07.svg"
+  },
+  {
+    "revision": "e84c205576ce51cc6a90653c49df11f0",
+    "url": "/static/media/curology-min.e84c2055.png"
+  },
+  {
+    "revision": "379bad944ff3f73142989e87c5b24773",
+    "url": "/static/media/lumin-min.379bad94.png"
+  },
+  {
+    "revision": "942c81fd7c87df7da8c098906a29d3fe",
+    "url": "/static/media/up-arrow-circle.942c81fd.svg"
+  },
+  {
+    "revision": "5f9e30771ee4dbbad1689af16cb49028",
+    "url": "/static/media/yourspace-min.5f9e3077.png"
+  }
+]);
